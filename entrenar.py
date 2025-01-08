ee6207de6de7ca3f6c78a2c7f12b4ea24686e09c
brayan-1a <i2414619@continental.edu.pe>
2025-01-07 20:31:29 -0500

    modelooo4

--- entrenar.py
+++ entrenar.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 # URL y API Key de Supabase
 URL = 'https://odlosqyzqrggrhvkdovj.supabase.co'
-KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbG9zcXl6cXJnZ3Jodmtkb3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjgyODksImV4cCI6MjA0NTY0NDI4OX0.z5btFX44Eu30kOBJj7eZKAmOUG62IrTcpXUVhMqK9Cky'  # Reemplaza con tu clave API
+KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbG9zcXl6cXJnZ3Jodmtkb3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjgyODksImV4cCI6MjA0NTY0NDI4OX0.z5btFX44Eu30kOBJj7eZKAmOUG62IrTcpXUVhMqK9Ck'  # Reemplaza con tu clave API
 
 def get_supabase_client():
     """Crea y devuelve una instancia del cliente Supabase."""
